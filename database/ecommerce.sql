cd58fbe79d20add0fe2b7c11f3285e9fd6797514
Olti <gashi.oltion@hotmail.com>
2021-04-07 00:42:34 +0200

    switched conn to db from localhost to ...

--- database/ecommerce.sql
+++ database/ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 20, 2021 at 06:01 PM
+-- Generation Time: Apr 07, 2021 at 12:25 AM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 7.4.15
 
@@ -20,20 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `ecommerce`
 --
-
-DELIMITER $$
---
--- Procedures
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `insertUserInfo` (IN `inputemail` VARCHAR(50), IN `inputpassword_` VARCHAR(100), IN `inputfname` VARCHAR(35), IN `inputlast_name` VARCHAR(35), IN `inputroleID` INT, IN `inputage` SMALLINT, IN `inputgender` VARCHAR(15), IN `inputadress` VARCHAR(250), IN `inputcity` VARCHAR(50), IN `inputcountry` VARCHAR(50), IN `inputpostal_code` INT, IN `inputphone_nr` INT)  BEGIN
-	INSERT INTO user_ (email, password_, fname, last_name, roleID)
-    VALUES (inputemail, inputpassword_, inputfname, inputlast_name, inputroleID);
-    
-    INSERT INTO personal_info (userID, age, gender, adress, city, country, postal_code, phone_nr)
-    VALUES (LAST_INSERT_ID(), inputage, inputgender, inputadress, inputcity, inputcountry, inputpostal_code, inputphone_nr);
-END$$
-
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -73,6 +59,16 @@ CREATE TABLE `chat` (
   `message` text DEFAULT NULL,
   `time_delivered` time DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`chatID`, `from_user_id`, `to_user_id`, `message`, `time_delivered`) VALUES
+(1, 5, 11, 'hello sir', '04:53:34'),
+(2, 5, 11, 'how you doing?', '04:53:45'),
+(3, 5, 11, 'Hello sir?', '02:30:51'),
+(4, 5, 11, 'haaaaaaaaaai men', '07:07:06');
 
 -- --------------------------------------------------------
 
@@ -117,6 +113,15 @@ CREATE TABLE `favouritelist` (
   `productID` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `favouritelist`
+--
+
+INSERT INTO `favouritelist` (`flID`, `userID`, `productID`) VALUES
+(1, 5, 64),
+(2, 5, 76),
+(3, 5, 78);
+
 -- --------------------------------------------------------
 
 --
@@ -135,7 +140,16 @@ CREATE TABLE `order_` (
 
 INSERT INTO `order_` (`orderID`, `userID`, `paid`) VALUES
 (22, 5, 1),
-(23, 5, 1);
+(23, 5, 1),
+(24, 14, 1),
+(25, 14, 1),
+(26, 5, 1),
+(27, 5, 1),
+(28, 5, 1),
+(29, 5, 1),
+(30, 5, 1),
+(31, 5, 1),
+(32, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -159,7 +173,18 @@ INSERT INTO `order_details` (`orderID`, `productID`, `quantity`, `date_ordered`)
 (22, 75, 1, '2021-03-17 18:04:16'),
 (23, 89, 1, '2021-03-19 17:24:28'),
 (23, 77, 1, '2021-03-19 17:24:28'),
-(23, 74, 1, '2021-03-19 17:24:28');
+(23, 74, 1, '2021-03-19 17:24:28'),
+(24, 83, 1, '2021-03-21 16:08:52'),
+(25, 65, 1, '2021-03-21 16:16:09'),
+(26, 66, 1, '2021-03-30 17:06:27'),
+(27, 65, 3, '2021-04-04 11:22:00'),
+(28, 65, 3, '2021-04-04 11:24:25'),
+(29, 76, 5, '2021-04-04 11:30:42'),
+(30, 76, 3, '2021-04-04 11:41:09'),
+(31, 64, 1, '2021-04-04 11:42:29'),
+(31, 65, 1, '2021-04-04 11:42:29'),
+(31, 66, 1, '2021-04-04 11:42:29'),
+(32, 80, 8, '2021-04-05 11:36:24');
 
 -- --------------------------------------------------------
 
@@ -182,7 +207,16 @@ CREATE TABLE `payment` (
 
 INSERT INTO `payment` (`payment_ID`, `orderID`, `userID`, `cart_type`, `total_price`, `date_`) VALUES
 (12, 22, 5, 'Visa', '1029.98', '2021-03-17 18:04:16'),
-(13, 23, 5, 'Westernunion', '1879.97', '2021-03-19 17:24:28');
+(13, 23, 5, 'Westernunion', '1879.97', '2021-03-19 17:24:28'),
+(14, 24, 14, 'Mastercard', '99.99', '2021-03-21 16:08:52'),
+(15, 25, 14, 'Mastercard', '699.99', '2021-03-21 16:16:09'),
+(16, 26, 5, 'Mastercard', '1899.99', '2021-03-30 17:06:27'),
+(17, 27, 5, 'Mastercard', '2099.97', '2021-04-04 11:22:00'),
+(18, 28, 5, 'Mastercard', '2099.97', '2021-04-04 11:24:25'),
+(19, 29, 5, 'Visa', '5999.95', '2021-04-04 11:30:42'),
+(20, 30, 5, 'Mastercard', '3599.97', '2021-04-04 11:41:09'),
+(21, 31, 5, 'Mastercard', '3699.97', '2021-04-04 11:42:29'),
+(22, 32, 13, 'Visa', '4799.92', '2021-04-05 11:36:24');
 
 -- --------------------------------------------------------
 
@@ -207,21 +241,19 @@ CREATE TABLE `personal_info` (
 
 INSERT INTO `personal_info` (`userID`, `age`, `gender`, `adress`, `city`, `country`, `postal_code`, `phone_nr`) VALUES
 (5, 30, 'male', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '49531532'),
-(6, 30, 'Male', '70 Grange Road London NW35 7QJ', 'London', 'United Kingdom', 'N5V 1A6', '49531532'),
 (1, 30, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '+38349567543'),
 (9, 30, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '49853124'),
 (10, 30, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '49752153'),
 (11, 30, 'male', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '195338132'),
-(12, 30, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '175374933'),
 (13, 25, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'United Kingdom', 'N5V', '447989023612'),
 (5, 30, 'male', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '49531532'),
-(6, 30, 'Male', '70 Grange Road London NW35 7QJ', 'London', 'United Kingdom', 'N5V 1A6', '49531532'),
 (1, 30, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '+38349567543'),
 (9, 30, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '49853124'),
 (10, 30, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '49752153'),
 (11, 30, 'male', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '195338132'),
-(12, 30, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'UK', 'N5V 1A6', '175374933'),
-(13, 25, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'United Kingdom', 'N5V', '447989023612');
+(13, 25, 'Female', '70 Grange Road London NW35 7QJ', 'London', 'United Kingdom', 'N5V', '447989023612'),
+(14, 23, 'male', '681 Glen Creek Drive\r\nBrooklyn, NY 11221', 'New York', 'USA', '11221', '1354648646'),
+(15, 29, 'male', 'blablablablabla', 'Ferizaj', 'Kosovo', '70000', '049475523');
 
 -- --------------------------------------------------------
 
@@ -243,18 +275,18 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`productID`, `product_name`, `price`, `quantity`, `category_name`, `company_name`) VALUES
-(64, 'Samsung Galaxy Note 20 Ultra 5G', '1099.99', 5, 'SmartPhone', 'Samsung'),
-(65, 'PlayStation 5', '699.99', 7, 'Console', 'PlayStation'),
-(66, 'Laptop HP i9, 16GB Ram, RTX 3070 ', '1899.99', 3, 'Laptop', 'HP'),
+(64, 'Samsung Galaxy Note 20 Ultra 5G', '1099.99', 0, 'SmartPhone', 'Samsung'),
+(65, 'PlayStation 5', '699.99', 6, 'Console', 'PlayStation'),
+(66, 'Laptop HP i9, 16GB Ram, RTX 3070 ', '1899.99', 2, 'Laptop', 'HP'),
 (67, 'Samsung Galaxy S20 FE 5G 256GB ', '499.99', 4, 'SmartPhone', 'Samsung'),
 (68, 'Samsung TV Ultra HD 49\"', '699.99', 3, 'TV', 'Samsung'),
 (74, 'Monitor DELL 24\'\' 144Ghz', '279.99', 5, 'Monitor', 'Dell'),
 (75, 'Xbox Series S 500 GB Black', '329.99', 5, 'Console', 'Xbox'),
-(76, 'Iphone 11 PRO Max 256 GB', '1199.99', 11, 'SmartPhone', 'Apple'),
+(76, 'Iphone 11 PRO Max 256 GB', '1199.99', 6, 'SmartPhone', 'Apple'),
 (77, 'Huawei P40 PRO 256 GB', '899.99', 5, 'SmartPhone', 'Huawei'),
 (78, 'PlayStation 4 Slim 500 GB ', '349.99', 4, 'Console', 'PlayStation'),
 (79, 'SAMSUNG Gaming Monitor 244 GHZ 49\"', '599.99', 4, 'Monitor', 'Samsung'),
-(80, 'Iphone SE 2 64GB', '599.99', 8, 'SmartPhone', 'Apple'),
+(80, 'Iphone SE 2 64GB', '599.99', 0, 'SmartPhone', 'Apple'),
 (81, 'Laptop ASUS ZenBook Duo , 14 \", Intel Core i7, 8GB ', '1599.99', 6, 'Laptop', 'Asus'),
 (82, 'Storm AMD Unbelievable (Ryzen 9 5950X, 32GB 4x8GB 4000MHz ', '3499.99', 3, 'Computer', 'Asus'),
 (83, 'Logitech MX Anywhere 2S Graphite, Wireless Mouse', '99.99', 6, 'Accessories', 'LogiTech'),
@@ -336,6 +368,14 @@ CREATE TABLE `shoppingcart` (
   `quantity` int(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `shoppingcart`
+--
+
+INSERT INTO `shoppingcart` (`shcID`, `userID`, `productID`, `quantity`) VALUES
+(14, NULL, 68, 1),
+(27, 5, 80, 8);
+
 -- --------------------------------------------------------
 
 --
@@ -354,8 +394,16 @@ CREATE TABLE `tracking` (
 --
 
 INSERT INTO `tracking` (`time_stamp`, `orderID`, `userID`, `status`) VALUES
-('2021-03-17 18:04:16', 22, 5, 1),
-('2021-03-19 17:24:28', 23, 5, 1);
+('2021-03-30 12:48:04', 22, 5, 3),
+('2021-03-21 13:11:19', 23, 5, 3),
+('2021-03-21 16:18:28', 25, 14, 3),
+('2021-03-30 17:06:27', 26, 5, 1),
+('2021-04-04 11:22:00', 27, 5, 1),
+('2021-04-04 11:24:25', 28, 5, 1),
+('2021-04-04 11:30:42', 29, 5, 1),
+('2021-04-04 11:41:09', 30, 5, 1),
+('2021-04-04 11:42:29', 31, 5, 1),
+('2021-04-05 11:36:24', 32, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -379,12 +427,12 @@ CREATE TABLE `user_` (
 INSERT INTO `user_` (`userID`, `email`, `password_`, `fname`, `last_name`, `roleID`) VALUES
 (1, 'gencgashi@gmail.com', '$2y$10$/O2.qQZK60C1Y1wjFd7GquCVHDNwHz5vSRk7FtK3Bo1U9g5W41OAK', 'Genc', 'Gashi', 1),
 (5, 'olti.g@hotmail.com', '$2y$10$HAQ/iSryPHEsgX9vtsvN2eR./tq3nAQuNu/JIOqnIlLesMolnCUjO', 'Oltion', 'Gashi', 3),
-(6, 'filan@gmail.com', '$2y$10$qDklBg7hZHU66oenZK/g..K36OGFdBNXIVNKbI0fzRGfu1c04bsDW', 'Filan', 'Fisteku', 3),
 (9, 'blerimdestani@gmail.com', '$2y$10$HHvM37aNMe55..7r6ongHu8FgCq6EPmqSg.fG7goA6CJ.20OToaPa', 'Blerim', 'Destani', 3),
 (10, 'destanbytyqi@gmail.com', '$2y$10$yDUtxJyUOeAcfitOqZjhpuA2vowtInsDevpe5CY6Ldvwm/3dvhvR2', 'Blerim', 'Destani', 3),
 (11, 'adriankrasniqi@hotmail.com', '$2y$10$1gdWif7T8vOkQDcjEOPhWOARs9UL/PqFI8kG83PCWgKa7tKpNnOA6', 'Adrian', 'Krasniqi', 2),
-(12, 'joewilliam@gmail.com', '$2y$10$x8xk7rn8Uoq4dw4kAjgCSe1gvpOV3qbYasaPkrHM0wuIDSOEDHPUO', 'Joe', 'William', 3),
-(13, 'oliviajones@gmail.com', '$2y$10$P3fm.iPhNwhETX7l4QFJoeYC/l4V0QSVIT6dL7F4gQqrpAhvRh2k2', 'Olivia', 'Jones', 3);
+(13, 'oliviajones@gmail.com', '$2y$10$P3fm.iPhNwhETX7l4QFJoeYC/l4V0QSVIT6dL7F4gQqrpAhvRh2k2', 'Olivia', 'Jones', 3),
+(14, 'filan@gmail.com', '$2y$10$RjoB3m1JkT5/82PxDYT.2OC4H6n0onypTts5NDr.XWb60.vnKmzwO', 'Filan', 'Fisteku', 3),
+(15, 'agonkurtaj@gmail.com', '$2y$10$MfqQ3RqZUXTSdNNqjfbMR.4GrY3lshGrYpLRBsPorCiS5.4.2U2kK', 'Agon', 'Kurtaj', 3);
 
 --
 -- Indexes for dumped tables
@@ -497,25 +545,25 @@ ALTER TABLE `user_`
 -- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `chatID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `chatID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `favouritelist`
 --
 ALTER TABLE `favouritelist`
-  MODIFY `flID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `flID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `order_`
 --
 ALTER TABLE `order_`
-  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 
 --
 -- AUTO_INCREMENT for table `payment`
 --
 ALTER TABLE `payment`
-  MODIFY `payment_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `payment_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `product`
@@ -539,13 +587,13 @@ ALTER TABLE `role_`
 -- AUTO_INCREMENT for table `shoppingcart`
 --
 ALTER TABLE `shoppingcart`
-  MODIFY `shcID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `shcID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT for table `user_`
 --
 ALTER TABLE `user_`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Constraints for dumped tables
--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,15 +1,30 @@
 <?php
 
-$serverName = "localhost";
-$dbUserName = "root";
-$dbPassword = "";
-$dbName = "ecommerce";
+// $serverName = "localhost";
+// $dbUserName = "root";
+// $dbPassword = "";
+// $dbName = "ecommerce";
 
-$conn = mysqli_connect($serverName, $dbUserName, $dbPassword, $dbName);
+// $conn = mysqli_connect($serverName, $dbUserName, $dbPassword, $dbName);
+
+// if(!$conn) {
+//     die("Connection failed: " . mysqli_connect_error());
+// } 
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 if(!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-} 
+    die("Connection failed " . mysqli_connect_error());
+}
 
 
 ?>
