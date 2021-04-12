8be924462971419cb7812f8977e5a1c63f813233
Oltion Gashi <gashi.oltion@hotmail.com>
2021-04-12 22:53:52 +0200

    did some changes to the code

--- includes/getmessageclient.inc.php
+++ includes/getmessageclient.inc.php
@@ -13,12 +13,12 @@ error_reporting(0); // hide error messages
         if($_SESSION['role'] == 2) {
 
             require_once "dbh.inc.php";
+            $output = "";
 
-            echo "<div class='main-wrapper'>";
-            echo "<div class='header-chat-wrapper' id='header-chat-wrapper'>";
+            $output .= "<div class='main-wrapper' id='main-wrapper'>";
+            // $output .= "<div class='header-chat-wrapper' id='header-chat-wrapper'>";
 
             //SELECTING USER TO CHAT
-            $output = "";
             $query = "SELECT * FROM user_ WHERE user_.userID = ?";
 
             $stmt = mysqli_stmt_init($conn);
@@ -37,7 +37,7 @@ error_reporting(0); // hide error messages
                     $firstName = $row['fname'];
                     $lastName = $row['last_name'];
 
-                    echo "
+                    $output .= "
                                     <div class='main-top'>
                                         <div class='main-top-img'>
                                             <i class='fas fa-user fa-2x'></i>
@@ -58,8 +58,9 @@ error_reporting(0); // hide error messages
                                     </div>";
                 }
                 // echo $output;
-            } else {
-                echo "<div class='main-top'>
+            } 
+            else {
+                $output .= "<div class='main-top'>
                                 <h4>Select chat to message</h4>
                             </div>";
             }
@@ -73,12 +74,8 @@ error_reporting(0); // hide error messages
                 $message = $_POST['message'];
             }
             
-            
-            // echo "<br><br><br><br><br>";
-            echo "<div class='main-bottom' id='main-bottom'>";   
+            $output .= "<div class='main-bottom' id='main-bottom'>";   
 
-            
-            $output2 = "";
             
             // WHERE c.to_user_id = u.userID OR c.from_user_id = u.userID AND c.to_user_id = ? ORDER BY c.chatID
             $query2 = "SELECT * FROM chat LEFT JOIN user_ ON user_.userID = chat.to_user_id
@@ -101,7 +98,7 @@ error_reporting(0); // hide error messages
                     $time = $row['time_delivered'];
                     $timeStamp = date('h:i A', strtotime($time));
                     if($row['from_user_id'] === $userID) {
-                       echo "<div class='outgoing-message-wrapper'>
+                        $output .= "<div class='outgoing-message-wrapper'>
                                 <div class='outgoing-message'>
                                         <div class='mine-info'>
                                             <span class='name'>Me</span>
@@ -125,7 +122,7 @@ error_reporting(0); // hide error messages
                             //                     </div>
                             //             </div>";
                     } else {
-                        echo "<div class='incoming-message'>
+                        $output .= "<div class='incoming-message'>
                                         <div class='messenger-info'>
                                             <span class='name'>$firstName $lastName</span>
                                             <span class='time'>$timeStamp</span>
@@ -148,25 +145,24 @@ error_reporting(0); // hide error messages
                     }
                 }
             } 
-            // echo $output2;
-            echo "</div>"; //close main-bottom div
-            echo "</div>";//close header-chat-wrapper
-            echo "<form class='myForm' id='myForm' autocomplete='off'>
-                        <div class='main-deliver-message'>
-                            <input type='hidden' class='sender' name='sender' value='$userID'>
-                            <input type='hidden' class='receiver' name='receiver' value='$receiverID'>
-                            <button type='submit' name='send' class='img'><i class='fas fa-paperclip'></i></button>
-                            <button type='submit' name='send' class='mic'><i class='fas fa-microphone-alt'></i></button>
-                            <input type='text' name='message' class='deliver-message' placeholder='Type here...'>
-                            <button type='submit' name='send' class='send' onclick='myForm()'><i class='fas fa-paper-plane'></i></button>
-                        </div>
-                    </form>";
-            echo "</div>";//close main-wrapper
-            // echo "<script src='JS/jquery-3.5.1.js'></script>
-            //         <script src='JS/helpagentchat.js'></script>";
-
+            // echo $output;
+            $output .= "</div>"; //close main-bottom div
+            // $output .= "</div>";//close header-chat-wrapper
+            $output .= "<form class='myForm' id='myForm' autocomplete='off'>
+                            <div class='main-deliver-message'>
+                                <input type='hidden' class='sender' name='sender' value='$userID'>
+                                <input type='hidden' class='receiver' name='receiver' value='$receiverID'>
+                                <button type='submit' name='send' class='img'><i class='fas fa-paperclip'></i></button>
+                                <button type='submit' name='send' class='mic'><i class='fas fa-microphone-alt'></i></button>
+                                <input type='text' name='message' id='deliver-message' class='deliver-message' placeholder='Type here...'>
+                                <button type='submit' name='send' class='send' id='send' onclick='myForm()'><i class='fas fa-paper-plane'></i></button>
+                            </div>
+                        </form>";
+            $output .= "</div>";//close main-wrapper
+            echo $output;
         }
     }
+
     ?>
     <!-- <button type='submit' name='send' class='send'><i class='fa fa-paper-plane' aria-hidden='true'></i></button> -->
     <!-- <input type='submit' class='send' name='send' value='&#xf1d8;' onclick='myForm()'> -->
@@ -176,22 +172,28 @@ error_reporting(0); // hide error messages
     <!-- <script src="JS/helpagentchat.js"></script> -->
     <script>
 
-        // const form = document.querySelector(".myForm"),
-        // sendingTo = form.querySelector(".receiver").value,
-        // inputField = form.querySelector(".deliver-message"),
-        // sendBtn = form.querySelector(".send"),
-        // const chatBox = document.querySelector(".main-bottom");
+        // const form = document.querySelector(".myForm");
+        // const sendingTo = document.querySelector(".receiver").value,
+        // inputField = document.querySelector(".deliver-message"),
+        // sendBtn = document.querySelector(".send"),
+        // chatBox = document.querySelector(".main-bottom");
+
+        // form.onsubmit() = (e) => {
+        //     e.preventDefault();
+        // }
+        
 
         function myForm() {
             document.getElementById("myForm").onsubmit = function(e) {
                 e.preventDefault();
+            }
 
-                var form = document.querySelector('.myForm');
-                var value = document.querySelector('.receiver').value;
-                var inputField = form.querySelector('.deliver-message');
+                const form = document.querySelector('.myForm'),
+                sendingTo = form.querySelector('.receiver').value,
+                inputField = form.querySelector('.deliver-message');
                 
                 let xhr = new XMLHttpRequest(); //creating XML object
-                xhr.open("POST", "includes/sendmessagehelpagent.inc.php?sendingto="+value, true);
+                xhr.open("POST", "includes/sendmessagehelpagent.inc.php?sendingto="+sendingTo, true);
                 xhr.onload = ()=>{
                     if(xhr.readyState === XMLHttpRequest.DONE) {
                         if(xhr.status === 200) {
@@ -203,23 +205,24 @@ error_reporting(0); // hide error messages
                 // We have to send the form data through ajax to php
                 let data = new FormData(form); //creating new formData Object
                 xhr.send(data); //sending the form data to php
-            }
         }  
         
         
         setInterval(() =>{
-            const form = document.querySelector(".myForm");
-            var chatBox = document.getElementById("main-bottom");
+            const form = document.querySelector(".myForm"),
+            sendingTo = form.querySelector('.receiver').value;
+            // var chatBox = document.getElementById("main-bottom");
+            var chatBox = document.getElementById("main-wrapper");
             // var getData = document.getElementById("main-bottom").innerHtml;
 
             let xhr = new XMLHttpRequest();
             xhr.open("POST", "includes/getmessageclient.inc.php", true);
-            xhr.onload = ()=>{
+            xhr.onload = () => {
                 if(xhr.readyState === XMLHttpRequest.DONE){
                     if(xhr.status === 200){
-                        // let data = xhr.response;
+                        let data = xhr.response;
                         // console.log(data);
-                        // chatBox.innerHTML = data;
+                        chatBox.innerHTML = data;
                         scrollToBottom();
                     }
                 }
@@ -228,7 +231,7 @@ error_reporting(0); // hide error messages
             xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             let formData = new FormData(form);
             xhr.send(formData);
-        }, 1500);
+        }, 15000);
 
         function getMessages() {
             // Prior to getting your messages.
@@ -247,7 +250,7 @@ error_reporting(0); // hide error messages
 
         scrollToBottom();
 
-        setInterval(getMessages, 1500);
+        setInterval(getMessages, 15000);
 
         
     </script>
